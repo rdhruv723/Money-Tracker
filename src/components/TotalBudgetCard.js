6e30a033f23324b2b8acf99c8b7d5428151f3f0e
rdhruv723 <96113268+rdhruv723@users.noreply.github.com>
2022-06-25 22:51:23 +0530

    Add files via upload

---
+++ src/components/TotalBudgetCard.js
@@ -0,0 +1,24 @@
+import { useBudgets } from "../contexts/BudgetsContext"
+import BudgetCard from "./BudgetCard"
+
+
+function TotalBudgetCard() {
+
+    const { expenses,budgets } = useBudgets()
+    const amount = expenses.reduce(
+      (total, expense) => total + expense.amount, 0
+    )
+
+    const max = budgets.reduce(
+        (total,budget) => total + budget.max, 0
+      )
+
+    if (max === 0) return null
+
+  return (
+   <BudgetCard gray name = "Total" amount = {amount} max = {max} hideButtons />
+
+  )
+}
+
+export default TotalBudgetCard
